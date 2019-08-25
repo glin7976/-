8f2d277bed19c630df70d138b4c9fc4f6c98caca
lin.gao <lin.gao@envision-digital.com>
2019-08-25 23:15:19 +0800

    feat: remove public

--- public/styles-4a90e5322a93aba83808.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{199:function(n,w,o){}}]);
-//# sourceMappingURL=styles-4a90e5322a93aba83808.js.map
--- public/styles-5cd8df763c74baaad48b.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{199:function(n,w,o){}}]);
-//# sourceMappingURL=styles-5cd8df763c74baaad48b.js.map
