ec867f7bc1375f0bc30cefaa3ae63503495029c0
lin.gao <lin.gao@envision-digital.com>
2019-08-26 18:41:48 +0800

    Updates

---
+++ component---src-pages-about-js-669c83689893c470a930.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{191:function(e,t,n){"use strict";n.r(t);var a=n(0),l=n.n(a);t.default=function(){return l.a.createElement("main",null,l.a.createElement("h1",null,"About the Author"),l.a.createElement("p",null,"Welcome to my Gatsby site."))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-669c83689893c470a930.js.map
---
+++ styles-a95b815bb02aa7e498c7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{201:function(n,w,o){}}]);
+//# sourceMappingURL=styles-a95b815bb02aa7e498c7.js.map
